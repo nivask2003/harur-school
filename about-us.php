0f4ca3f76991dfaf84ef1ad457c45cd7be437813
Sasi <sasidharankm4@gmail.com>
2026-08-24 18:13:37 +0530

    File Updated

---
+++ about-us.php
@@ -0,0 +1,45 @@
+<?php include "function.php" ?>
+<?php template_header("About | Harur International School", "Discover Harur International School, a Senior Secondary CBSE school focused on academic excellence, character development, values, leadership, and holistic student growth.") ?>
+<secion class="page-banner" id="about">
+    <h1>About Us</h1>
+</secion>
+<section class="about-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-4" data-aos="fade-right">
+                <img src="assets/images/logo.webp" alt="logo" class="img-fluid logo">
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" data-aos="fade-down">
+                <span class="badge">School Profile</span>
+                <h2 class="section-title">A Legacy of Learning and Growth</h2>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" data-aos="fade-left">
+                <p class="section-desc">Harur International School, established in 2018, is a Senior Secondary CBSE institution committed to quality education in a safe and inspiring environment. Through academic excellence, innovative teaching, individual attention, technology-enabled learning, sports, cultural activities, and leadership opportunities, the school nurtures confident, responsible, compassionate, and globally aware learners.</p>
+            </div>
+        </div>
+        <div class="image-container" data-aos="fade-up">
+            <img src="assets/images/about-img.webp" alt="" class="img-fluid">
+        </div>
+    </div>
+</section>
+<section class="about-2 mt-5" data-aos="fade-right">
+    <div class="container">
+        <img src="assets/images/vision-mission.webp" alt="" class="img-fluid">
+    </div>
+</section>
+<section class="about-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6" data-aos="fade-right">
+                <span class="badge">Why Choose Us</span>
+                <h2 class="section-title">Discover What Makes Us Special!</h2>
+                <p class="section-desc">Harur International School offers a CBSE Senior Secondary curriculum from Kindergarten to Grade XII, supported by experienced, caring, and committed faculty. The school focuses on student-centred teaching, individual attention, academic achievement, conceptual understanding, and integrated NEET/JEE preparation.</p>
+                <p class="section-desc">Technology-enabled, experiential, and STEM-based learning is complemented by value-based education, character development, and a safe, secure, hygienic campus. Students benefit from sports, arts, cultural activities, clubs, leadership opportunities, career guidance, counselling, communication-skill development, active parent partnership, and 21st-century skills.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6" data-aos="fade-left">
+                <img src="assets/images/why-choose.webp" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+<?php template_footer()?>
--- function.php
+++ function.php
@@ -119,7 +119,7 @@ function template_header($title, $metadesc)
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
                                 </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -127,7 +127,7 @@ function template_header($title, $metadesc)
                                         About
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="about-us.php">About Us</a></li>
                                         <li><a class="dropdown-item" href="#">Another action</a></li>
                                         <li>
                                             <hr class="dropdown-divider">
