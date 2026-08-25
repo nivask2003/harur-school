dc0636eb09fa893f9c508e05756effbe492d9274
Sasi <sasidharankm4@gmail.com>
2026-08-25 17:23:22 +0530

    File Updated

--- contact.php
+++ contact.php
@@ -1,3 +1,75 @@
 <?php include "function.php" ?>
-<?php template_header("Contact | Harur International School","") ?>
-<?php template_footer()?>
+<?php template_header("Contact | Harur International School", "") ?>
+<section class="page-banner" id="contact">
+    <h1>Contact</h1>
+</section>
+<section class="contact-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="icon-box">
+                    <div class="row">
+                    <div class="col-12 col-md-3 col-lg-3">
+                        <div class="icon">
+                            <i class="bi bi-geo-alt"></i>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-7 col-lg-7">
+                        <div class="content">
+                            <h5>Address</h5>
+                            <p>Harur - Salem Main Road,
+                                H. Doddampatti, Harur,
+                                Dharmapuri District - 636 903</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                
+
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="icon-box">
+                    <div class="row">
+                    <div class="col-12 col-md-3 col-lg-3">
+                        <div class="icon">
+                            <i class="bi bi-telephone"></i>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-7 col-lg-7">
+                        <div class="content">
+                            <h5>Phone Number</h5>
+                            <p><a href="tel:+917558146222">+91 7558146222</a></p>
+                            <p><a href="tel:+917558145222">+91 7558145222</a></p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                
+
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="icon-box">
+                    <div class="row">
+                    <div class="col-12 col-md-3 col-lg-3">
+                        <div class="icon">
+                            <i class="bi bi-envelope"></i>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-7 col-lg-7">
+                        <div class="content">
+                            <h5>Email </h5>
+                            <p><a href="admission@harurinternationalschool.com">admission@harurinternationalschool.com</a></p>
+                            <p><a href="principal@harurinternationalschool.com">principal@harurinternationalschool.com</a></p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="contact-2"></section>
+<?php template_footer() ?>
