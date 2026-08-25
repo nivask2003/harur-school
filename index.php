66eb99b89fc0e8de807b1f4ef0f8c07e37433fd1
Nivas Kumar S <nivask457@gmail.com>
2026-08-25 22:46:37 +0530

    File Updated

--- index.php
+++ index.php
@@ -18,6 +18,14 @@
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <style>
+        html, body {
+            height: 100%;
+            overflow: hidden;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 
 <body>
