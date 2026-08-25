665560511dbd6b7cf1bf0f71096f20312ea8bd08
Sasi <sasidharankm4@gmail.com>
2026-08-25 18:24:50 +0530

    File Updated

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
-<body>
+<body class="no-scroll">
     <section class="landing-page">
         <div class="container">
             <div class="box">
