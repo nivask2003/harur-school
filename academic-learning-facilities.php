b15e6a5929e9c7bee02cbe7f659904fdedbcc2ca
Sasi <sasidharankm4@gmail.com>
2026-09-03 16:24:41 +0530

    File Updated

--- academic-learning-facilities.php
+++ academic-learning-facilities.php
@@ -5,6 +5,77 @@
 </section>
 <section class="fac-1">
     <div class="container">
-        
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Smart classroom with interactive digital display" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Smart Classroom</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Modern computer laboratory with student workstations" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Modern Computer Laboratory</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Science laboratory equipped for practical experiments" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Science Laboratories</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Air-conditioned classroom for comfortable learning" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Air-Conditioned Classrooms</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Mathematics laboratory with learning aids and models" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Mathematics Laboratory</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Language learning facility with audio-visual resources" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Language Learning Facilities</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Language learning facility with audio-visual resources" class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Language Learning Facilities</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--- safety-health-student-support.php
+++ safety-health-student-support.php
@@ -5,6 +5,67 @@
 </section>
 <section class="fac-1">
     <div class="container">
-        
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Campus monitoring that supports student safety and security." class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>CCTV Surveillance</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Organised transport with clear expectations for safety and discipline." class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Safe Transport Facility</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="A clean and well-maintained space for students." class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Hygienic Cafeteria</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Basic assistance and first-aid support for student health needs." class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Medical Care & First Aid</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="Clean and appropriate facilities maintained for student comfort and hygiene." class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Separate Sanitary Facilities</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="box-image">
+                        <img src="assets/images/smart-classroom.webp" alt="A pleasant campus environment that promotes cleanliness, responsibility, and respect for nature." class="img-fluid">
+                    </div>
+                    <div class="box-header">
+                        <h4>Green & Eco-Friendly Campus</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
