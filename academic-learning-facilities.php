c1f4dbca2e0597fdedeb8e24ee86a8ef7e9e5b1b
Sasi <sasidharankm4@gmail.com>
2026-09-03 13:23:45 +0530

    File Updated

--- academic-learning-facilities.php
+++ academic-learning-facilities.php
@@ -1,7 +1,7 @@
 <?php include "function.php"?>
 <?php template_header("Academic & Learning Facilities | Harur International School", "Explore Academic & Learning Facilities at Harur International School, supporting quality education through modern classrooms, technology-enabled learning, science, and academic resources."); ?>
 <section class="page-banner" id="facilities">
-    <h1>Academic & Learning Facilities</h1>
+    <h1>Academic & <br>Learning Facilities</h1>
 </section>
 <section class="fac-1">
     <div class="container">
--- safety-health-student-support.php
+++ safety-health-student-support.php
@@ -1,7 +1,7 @@
 <?php include "function.php"?>
 <?php template_header("Safety, Health & Student Support | Harur International School", "Explore Safety, Health & Student Support at Harur International School, ensuring a secure, healthy, caring, and supportive environment for every student."); ?>
 <section class="page-banner" id="facilities">
-    <h1>Safety, Health & Student Support</h1>
+    <h1>Safety, Health & <br>Student Support</h1>
 </section>
 <section class="fac-1">
     <div class="container">
