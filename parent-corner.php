729462bea07c01ddfb978b5dc0771aaf31352718
Sasi <sasidharankm4@gmail.com>
2026-09-09 17:18:14 +0530

    File Updated

--- parent-corner.php
+++ parent-corner.php
@@ -3,4 +3,17 @@
 <section class="page-banner" id="parent-corner">
     <h1>Parent's Corner</h1>
 </section>
+<section class="parent-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+                <span class="badge">Parent's Corner</span>
+                <h2 class="section-title">Supporting Parents and Students</h2>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6">
+                <p class="section desc">The partnership between parents and the school plays an essential role in every child's progress. Parents and guardians are requested to support regular attendance, discipline, academic responsibility, healthy habits, respectful behaviour, and active participation in school life.</p>
+            </div>
+        </div>
+    </div>
+</section>
 <?php template_footer()?>
