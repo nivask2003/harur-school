661716928995a63aa0a3cd6252ee9ed6655a95f2
Sasi <sasidharankm4@gmail.com>
2026-09-10 17:42:11 +0530

    File Updated

--- parent-corner.php
+++ parent-corner.php
@@ -17,4 +17,235 @@
         <img src="assets/images/parent-corner.webp" alt="" class="img-fluid">
     </div>
 </section>
-<?php template_footer()?>
+<section class="parent-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+                <span class="badge">ACADEMIC SUPPORT</span>
+                <h2 class="section-title">Creating Better Learning Habits</h2>
+                <p class="section-desc">Parents can support academic progress at home by staying informed, encouraging good habits, maintaining routines, and communicating regularly with school.</p>
+                <ul class="icon-list">
+                    <li class="list">
+                        <div class="row">
+                            <div class="col-12 col-md-2 col-lg-2"><i class="bi bi-book"></i></div>
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <p>Carefully review the School Handbook and official school communications on a regular basis with care.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list">
+                        <div class="row">
+                            <div class="col-12 col-md-2 col-lg-2"><i class="bi bi-bar-chart-line"></i></div>
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <p>Examine report cards carefully and help the child improve in subjects needing extra attentions today.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list">
+                        <div class="row">
+                            <div class="col-12 col-md-2 col-lg-2"><i class="bi bi-journal-check"></i></div>
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <p>Check books and notebooks periodically, encouraging neat, complete, and timely work each school days.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list">
+                        <div class="row">
+                            <div class="col-12 col-md-2 col-lg-2"><i class="bi bi-calendar-check"></i></div>
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <p>Ensure that the child brings the correct books and materials according to the class timetables daily.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list">
+                        <div class="row">
+                            <div class="col-12 col-md-2 col-lg-2"><i class="bi bi-bookmark-check"></i></div>
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <p>Cover and label books at the start of each term and help the child maintain them neatly all the year.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list">
+                        <div class="row">
+                            <div class="col-12 col-md-2 col-lg-2"><i class="bi bi-chat-square-text"></i></div>
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <p>Acknowledge notes and instructions sent by the school through the Handbook or other approved channel.</p>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6 image">
+                <img src="assets/images/aca-home.webp" alt="" class="img-fluid">
+                <img src="assets/images/aca-home-1.webp" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+<div class="parent-3">
+    <div class="container">
+        <div class="heading-center">
+            <span class="badge">ATTENDANCE & COMMUNICATION</span>
+            <h2 class="section-title">Attendance, Communication, Visits</h2>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="row">
+                        <div class="col-12 col-md-3 col-lg-3">
+                            <div class="icon">
+                                <i class="bi bi-calendar-x"></i>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9 col-lg-9 content">
+                            <h4>Attendance & Records</h4>
+                            <span class="box-badge">Regular Attendance</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="row">
+                        <div class="col-12 col-md-3 col-lg-3">
+                            <div class="icon">
+                                <i class="bi bi-people"></i>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9 col-lg-9 content">
+                            <h4>PTM Attendance Guide</h4>
+                            <span class="box-badge">Parent-Teachers Meeting</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="row">
+                        <div class="col-12 col-md-3 col-lg-3">
+                            <div class="icon">
+                                <i class="bi bi-building-check"></i>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9 col-lg-9 content">
+                            <h4>Manage School Visits</h4>
+                            <span class="box-badge">School Visits</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <img src="assets/images/parent-3.webp" alt="" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="box">
+                    <div class="row">
+                        <div class="col-12 col-md-3 col-lg-3">
+                            <div class="icon">
+                                <i class="bi bi-chat-square-text"></i>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9 col-lg-9 content">
+                            <h4>Complaint Resolution</h4>
+                            <span class="box-badge">Concerns & Complaints</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="row">
+                        <div class="col-12 col-md-3 col-lg-3">
+                            <div class="icon">
+                                <i class="bi bi-telephone"></i>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9 col-lg-9 content">
+                            <h4>Keep Details Updated</h4>
+                            <span class="box-badge">Contact Information</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="row">
+                        <div class="col-12 col-md-3 col-lg-3">
+                            <div class="icon">
+                                <i class="bi bi-envelope-check"></i>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9 col-lg-9 content">
+                            <h4>Check School Updates</h4>
+                            <span class="box-badge">School Updates</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="parent-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+                <img src="assets/images/parent-4.webp" alt="" class="img-fluid">
+                <div class="video">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/jHlKM7E2wXE?si=PzZKcgbFMI0wRzGc&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6">
+                <span class="badge">UNIFORM, HEALTH & SAFETY</span>
+                <h2 class="section-title">Student Uniform Health & Safety</h2>
+                <p class="section-desc">Parents are requested to send children to school in a clean, neatly ironed uniform, with shoes polished and kept in good condition at all times. Costly or unnecessary items should not be sent to school under any circumstances.</p>
+                <p class="section-desc">A child suffering from an infectious illness should not be sent to school until fully recovered, in order to protect the health and safety of other students. Parents are also encouraged to consistently promote healthy habits, good personal hygiene, adequate daily water intake, and nutritious meals at home.</p>
+                <ul class="icon-list">
+                    <li class="list">
+                        <i class="bi bi-caret-right-square-fill"></i>
+                        <p><b>Neat School Uniform</b>: Children should attend school in a clean, neatly ironed uniform with well-maintained polished shoes.</p>
+                    </li>
+                    <li class="list">
+                        <i class="bi bi-caret-right-square-fill"></i>
+                        <p><b>Personal Belongings</b>: Costly or unnecessary items should not be sent to school under any circumstances.</p>
+                    </li>
+                    <li class="list">
+                        <i class="bi bi-caret-right-square-fill"></i>
+                        <p><b>Health & Illness</b>: Children with infectious illnesses should remain at home until fully recovered to protect others.</p>
+                    </li>
+                    <li class="list">
+                        <i class="bi bi-caret-right-square-fill"></i>
+                        <p><b>Personal Hygiene</b>: Parents should encourage good personal hygiene and healthy habits consistently at home.</p>
+                    </li>
+                    <li class="list">
+                        <i class="bi bi-caret-right-square-fill"></i>
+                        <p><b>Healthy Nutrition</b>: Nutritious meals and adequate daily water intake should be encouraged for children's wellbeing.</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="parent-5">
+    <div class="container">
+        <div class="heading-center">
+            <span class="badge">PARTICIPATION & FEES</span>
+            <h2 class="section-title">Encouraging Participation and Responsibility</h2>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Participation & Fees</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Encourage the child to participate actively in academic, sports, cultural, club, and service activities.</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Support punctuality, discipline, respectful behaviour, and responsible use of school property.</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Remit school fees within the prescribed schedule.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section>
+<?php template_footer() ?>
